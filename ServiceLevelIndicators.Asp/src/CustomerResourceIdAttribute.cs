f47a6c301046115ade38fca70fe1e80bc05e2e0a
Xavier <xavierjohn@hotmail.com>
2023-06-29 11:01:29 -0700

    Update CustomerResourceIdAttribute

--- ServiceLevelIndicators.Asp/src/CustomerResourceIdAttribute.cs
+++ ServiceLevelIndicators.Asp/src/CustomerResourceIdAttribute.cs
@@ -1,14 +1,8 @@
 ﻿namespace ServiceLevelIndicators.Asp;
 using System;
 
-[AttributeUsage(AttributeTargets.Field)]
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
 public class CustomerResourceIdAttribute : Attribute
 {
-    public CustomerResourceIdAttribute(string resourceId)
-    {
-        ResourceId = resourceId;
-    }
-
-    public string ResourceId { get; }
 }
 
