13ce807a01c7a9a01a1291e04e14ec78a5ff993a
Xavier <xavierjohn@hotmail.com>
2023-08-29 11:00:47 -0700

    API Versioning test

---
+++ ServiceLevelIndicators.Asp.ApiVersioning/tests/TestController.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceLevelIndicators.Asp.ApiVersioning.Tests;
+
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+[ApiVersion("2023-8-29")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await Task.Delay(1);
+        return Ok("Hello World!");
+    }
+}
