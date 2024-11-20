5402e0fb54fd50c80faaf39417ddfff492ca2781
Xavier <xavierjohn@hotmail.com>
2024-11-20 10:24:29 -0800

    HIde custom meter name option.

--- ServiceLevelIndicators/src/ServiceLevelIndicatorMeterProviderBuilderExtensions.cs
+++ ServiceLevelIndicators/src/ServiceLevelIndicatorMeterProviderBuilderExtensions.cs
@@ -10,8 +10,7 @@ public static class ServiceLevelIndicatorMeterProviderBuilderExtensions
     /// Enables Service Level Indicator instrumentation.
     /// </summary>
     /// <param name="builder"><see cref="MeterProviderBuilder"/> being configured.</param>
-    /// <param name="meterName"><see cref="string"/> if you are using customer meters, give that name.</param>
     /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
-    public static MeterProviderBuilder AddServiceLevelIndicatorInstrumentation(this MeterProviderBuilder builder, string? meterName = null)
-        => builder.AddMeter(meterName ?? ServiceLevelIndicator.DefaultMeterName);
+    public static MeterProviderBuilder AddServiceLevelIndicatorInstrumentation(this MeterProviderBuilder builder)
+        => builder.AddMeter(ServiceLevelIndicator.DefaultMeterName);
 }
