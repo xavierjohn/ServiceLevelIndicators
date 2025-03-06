50ee358dc5cc7148d6f1963d80e1e0d62dfc62a5
Xavier <xavierjohn@hotmail.com>
2025-03-06 12:13:24 -0800

    Sample SLI Console application (#47)
    * Sample SLI Console application
    
    Output:
    ```
    Metric Name: operation.duration, Duration of the operation., Unit: ms, Meter: ServiceLevelIndicator/8.0.0.0
    (2025-03-06T20:00:59.4535321Z, 2025-03-06T20:01:01.0792349Z] activity.status.code: Ok CustomerResourceId: SampleConsoleSLI LocationId: ms-loc://az/public/westus3 Operation: OperationWork Histogram
    Value: Sum: 1540 Count: 1 Min: 1540 Max: 1540
    (-Infinity,0]:0
    (0,5]:0
    (5,10]:0
    (10,25]:0
    (25,50]:0
    (50,75]:0
    (75,100]:0
    (100,250]:0
    (250,500]:0
    (500,750]:0
    (750,1000]:0
    (1000,2500]:1
    (2500,5000]:0
    (5000,7500]:0
    (7500,10000]:0
    (10000,+Infinity]:0
    ```

---
+++ sample/ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Azure.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using ServiceLevelIndicators;
+
+#pragma warning disable CA1848 // Use the LoggerMessage delegates
+
+const string ProgramName = "SampleConsoleSLI";
+string version = Assembly
+    .GetExecutingAssembly()
+    .GetCustomAttribute<AssemblyFileVersionAttribute>()!
+    .Version;
+ResourceBuilder resourceBuilder = ResourceBuilder
+    .CreateEmpty()
+    .AddService(ProgramName, serviceVersion: version, serviceInstanceId: Environment.MachineName);
+
+
+ServiceCollection services = new();
+services.Configure<ServiceLevelIndicatorOptions>(r =>
+{
+    r.CustomerResourceId = ProgramName; // Customer ID if this work is done on behalf of a customer.
+    r.LocationId = ServiceLevelIndicator.CreateLocationId("public", AzureLocation.WestUS3.Name);
+});
+
+services
+    .AddLogging(builder =>
+    {
+        builder.AddOpenTelemetry(options =>
+        {
+            options.SetResourceBuilder(resourceBuilder);
+            options.AddConsoleExporter();
+        });
+    })
+    .AddSingleton<ServiceLevelIndicator>(); ;
+
+var serviceProvider = services.BuildServiceProvider();
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+using MeterProvider meterProvider = Sdk.CreateMeterProviderBuilder()
+                .SetResourceBuilder(resourceBuilder)
+                .AddConsoleExporter()
+                .AddServiceLevelIndicatorInstrumentation()
+                .Build();
+
+var serviceLevelIndicator = serviceProvider.GetRequiredService<ServiceLevelIndicator>();
+using MeasuredOperation measuredOperation = serviceLevelIndicator.StartMeasuring("OperationWork");
+
+try
+{
+    logger.LogInformation("Starting to do some work...");
+    await Task.Delay(1000); // Simulate some work
+    logger.LogInformation("Work done.");
+    measuredOperation.SetActivityStatusCode(ActivityStatusCode.Ok);
+}
+catch (Exception ex)
+{
+    measuredOperation.SetActivityStatusCode(ActivityStatusCode.Error);
+    logger.LogError(ex, "An error occurred doing work.");
+}
